00ec0f7c0a1bec54d429aa2325dd7c01614e603d
Jair Escamilla <hernandez_yo14@outlook.es>
2020-07-11 14:46:53 -0500

    Slugs unicos

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from django.db.models.signals import pre_save
+import uuid
 
 # Create your models here.
 class Product(models.Model):
@@ -19,7 +20,11 @@ class Product(models.Model):
         return self.title
 
 def set_slug(sender, instance, *args, **kwargs): #callback
-    instance.slug = slugify(instance.title)
-
+    if instance.title and not instance.slug:
+        slug = slugify(instance.title)
+        
+        while Product.objects.filter(slug=slug).exists():
+            slug = slugify("{}-{}".format(instance.title, str(uuid.uuid4())[:8]))
+        instance.slug = slug
 
 pre_save.connect(set_slug, sender=Product)
