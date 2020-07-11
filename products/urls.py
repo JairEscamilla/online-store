e0b9e04b21491f4d0ae886d0d888679b8835e8f6
Jair Escamilla <hernandez_yo14@outlook.es>
2020-07-11 16:41:44 -0500

    Busqueda basada en multiples filtros

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<slug:slug>/", views.ProductDetailView.as_view(), name="product") #id -> Llave primaria
+    path("search/", views.ProductSearchListView.as_view(), name="search"),
+    path("<slug:slug>/", views.ProductDetailView.as_view(), name="product"), #id -> Llave primaria
 ]
--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Product
+from django.db.models import Q
 # Create your views here.
 
 class ProductListView(ListView):
@@ -21,4 +22,20 @@ class ProductDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         print(context)
+        return context
+
+class ProductSearchListView(ListView):
+    template_name = "products/search.html"
+
+    def get_queryset(self):
+        filters = Q(title__icontains=self.query()) | Q(category__title__icontains=self.query())
+        return Product.objects.filter(filters)
+    
+    def query(self):
+        return self.request.GET.get('q')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.query()
+        context['count'] = context['product_list'].count()
         return context
