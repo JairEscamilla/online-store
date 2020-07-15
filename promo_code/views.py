20d08ffbeb2b425f686901673ae8458d6ea0f81a
Jair Escamilla <hernandez_yo14@outlook.es>
2020-07-14 23:49:34 -0500

    Enviando códigos de promoción a través de AJAX

--- promo_code/views.py
+++ promo_code/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import PromoCode
 
 # Create your views here.
 def validate(request):
+    code = request.GET.get("code")
+
+    promo_code = PromoCode.objects.filter(code=code).first()
+
+    if promo_code is None:
+        return JsonResponse({
+            "status": False
+        }, status=404)
+    
+
+
     return JsonResponse({
-        'name': 'Jair',
-        'job': 'developer'
+        "status": True,
+        "code": promo_code.code,
+        "discount": promo_code.discount
     })
